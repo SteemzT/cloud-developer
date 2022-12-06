5c3c0073b234e91d26474ada841beb9b6132fd77
SteemzT <102822309+SteemzT@users.noreply.github.com>
2022-12-06 14:21:34 +0100

    Edited codebase to reflect best coding pratices

--- course-04/project/c4-final-project-starter-code/backend/src/businessLogic/todos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/businessLogic/todos.ts
@@ -1,70 +1,86 @@
 import { TodosAccess } from '../dataLayer/todosAcess'
+import { TodoUpdate } from '../models/TodoUpdate'
 import { AttachmentUtils } from '../helpers/attachmentUtils';
 import { TodoItem } from '../models/TodoItem'
+import * as uuid from 'uuid'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
+//import * as createError from 'http-errors'
 import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
 import { createLogger } from '../utils/logger'
-import * as uuid from 'uuid'
-import { TodoUpdate } from '../models/TodoUpdate'
-//import * as createError from 'http-errors'
+
 
 // TODO: Implement businessLogic
-const logger = createLogger('TodosAccess')
-const attachmentUtils = new AttachmentUtils()
+//Declare log variable
+const log = createLogger('TodosAccess')
+
+//Declare attachment_Utils variable
+const attachment_Utils = new AttachmentUtils()
+
+//Declare todosAccess variable
 const todosAccess = new TodosAccess()
 
 // Write get todos function
+// Implement async/await function for getTodosForUser
 export async function getTodosForUser(userId: string): Promise<TodoItem[]> {
-    logger.info('Get todos for user function called')
+  // log info message
+    log.info('Called get todos for user function')
     return todosAccess.getAllTodos(userId)
 }
 
 // Write create todo function
+// Implement async/await function for createTodo
 export async function createTodo(
   newTodo: CreateTodoRequest,
   userId: string
 ): Promise<TodoItem> {
-  logger.info('Create todo function called')
+  // log info message
+  log.info('Called create todo function')
 
   const todoId = uuid.v4()
   const createdAt = new Date().toISOString()
-  const s3AttachmentUrl = attachmentUtils.getAttachmentUrl(todoId)
-  const newItem = {
+  const s3_Attachment_Url = attachment_Utils.getAttachmentUrl(todoId)
+  const new_Item = {
     userId,
     todoId,
     createdAt,
     done: false,
-    attachmentUrl: s3AttachmentUrl,
+    attachmentUrl: s3_Attachment_Url,
     ...newTodo
   }
 
-  return await todosAccess.createTodoItem(newItem)
+  return await todosAccess.createTodoItem(new_Item)
 }
 
 // Write update todo function
+// Implement async/await function for updateTodo
 export async function updateTodo(
     todoId: string,
     todoUpdate: UpdateTodoRequest,
     userId: string
     ): Promise<TodoUpdate> {
-    logger.info('Update todo function called')
+    // log info message
+    log.info('Called update todo function')
     return todosAccess.updateTodoItem(todoId, userId, todoUpdate)
 }
 
 // Write delete todo function
+// Implement async/await function for deleteTodo
 export async function deleteTodo(
     todoId: string,
     userId: string
     ): Promise<string> {
-    logger.info('Delete todo function called')
+    // log info message
+    log.info('Called delete todo function')
     return todosAccess.deleteTodoItem(todoId, userId)
 }
 
 // Write create attachment function
+// Implement async/await function for createAttachmentPresignUrl
 export async function createAttachmentPresignedUrl(
     todoId: string,
     userId: string
     ): Promise<string> {
-    logger.info('Create attachment function called by user', userId, todoId)
-    return attachmentUtils.getUploadUrl(todoId)
+    // log info message
+    log.info('Called create attachment function by user', userId, todoId)
+    return attachment_Utils.getUploadUrl(todoId)
 }
--- course-04/project/c4-final-project-starter-code/backend/src/helpers/attachmentUtils.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/attachmentUtils.ts
@@ -1,16 +1,16 @@
 import * as AWS from 'aws-sdk'
 import * as AWSXRay from 'aws-xray-sdk'
 
-const XAWS = AWSXRay.captureAWS(AWS)
+const P_AWS = AWSXRay.captureAWS(AWS)
 
 // TODO: Implement the fileStogare logic
-const s3BucketName = process.env.ATTACHMENT_S3_BUCKET
+const s3_BucketName = process.env.ATTACHMENT_S3_BUCKET
 const urlExpiration = 300
 
 export class AttachmentUtils{
     constructor(
-        private readonly s3 = new XAWS.S3({ signatureVersion: 'v4' }),
-        private readonly bucketName = s3BucketName
+        private readonly s3 = new P_AWS.S3({ signatureVersion: 'v4' }),
+        private readonly bucketName = s3_BucketName
     ) {}
 
     getAttachmentUrl(todoId: string) {
